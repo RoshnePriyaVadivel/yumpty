b1575f7876dabad153831fc441f661ea5239dafd
RoshnePriyaVadivel <126015091@sastra.ac.in>
2024-06-30 23:54:04 +0530

    test commit

--- app/src/main/java/com/example/yumpty/MainActivity.kt
+++ app/src/main/java/com/example/yumpty/MainActivity.kt
@@ -1,30 +1,44 @@
 package com.example.yumpty
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+//import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.NavController
-import androidx.compose.ui.platform.LocalContext
+import com.example.yumpty.ui.theme.YumptyTheme
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.FloatingActionButton
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.material.FabPosition
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.material.TabRow
@@ -42,20 +56,10 @@ import androidx.compose.material.TextField
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Button
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.yumpty.ui.theme.YumptyTheme
-import androidx.compose.material.icons.filled.ArrowDropDown
-import android.content.Context
-import android.os.Environment
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
+import androidx.compose.material.Checkbox
+import androidx.compose.runtime.LaunchedEffect
+
 
 class MainActivity : ComponentActivity() {
     @ExperimentalComposeUiApi
@@ -71,6 +75,7 @@ class MainActivity : ComponentActivity() {
             YumptyApp()
         }
     }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 1) {
@@ -83,28 +88,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class FoodItem(val name: String, val price: Int)
+
 @ExperimentalComposeUiApi
 @Composable
 fun YumptyApp() {
     val navController = rememberNavController()
+    val context = LocalContext.current
+    val foodItems = remember { mutableStateOf<List<FoodItem>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        foodItems.value = readFoodItemsFromFile(context, "food_items.csv")
+    }
+
     NavHost(navController, startDestination = "main") {
-        composable("main") { Yumpty(navController) }
+        composable("main") { Yumpty(navController, foodItems.value) }
         composable("addItem") { AddItemScreen(navController) }
     }
 }
 
 @ExperimentalComposeUiApi
 @Composable
-fun Yumpty(navController: NavController) {
-    val context = LocalContext.current
-    val tabs = listOf("Food", "Fun")
-    val tabContent: @Composable (String) -> Unit = { tab ->
-        when (tab) {
-            "Food" -> FoodScreen()
-            "Fun" -> FunScreen()
-            else -> FoodScreen()
-        }
-    }
+fun Yumpty(navController: NavController, foodItems: List<FoodItem>) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -113,7 +118,13 @@ fun Yumpty(navController: NavController) {
         },
         content = {
             Column {
-                TabLayout(tabs, tabContent)
+                TabLayout(listOf("Food", "Fun")) { tab ->
+                    when (tab) {
+                        "Food" -> FoodScreen(foodItems = foodItems)
+                        "Fun" -> FunScreen()
+                        else -> FoodScreen(foodItems = foodItems)
+                    }
+                }
             }
         },
         floatingActionButton = {
@@ -160,35 +171,83 @@ fun TabLayout(
     }
 }
 
+
 @Composable
-fun FoodScreen() {
+fun FoodScreen(foodItems: List<FoodItem>) {
     Column {
-        Text(text = "Food Tab Content")
+        RecyclerView(
+            foodItems = foodItems,
+            selectedItems = emptySet(),
+            onItemCheckedChange = { _, _ -> }
+        )
+    }
+}
+
+@Composable
+fun RecyclerView(
+    foodItems: List<FoodItem>,
+    selectedItems: Set<FoodItem>,
+    onItemCheckedChange: (FoodItem, Boolean) -> Unit
+) {
+    LazyColumn {
+        items(foodItems) { item ->
+            ListItem(
+                item = item,
+                isChecked = selectedItems.contains(item),
+                onCheckedChange = { isChecked ->
+                    onItemCheckedChange(item, isChecked)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun ListItem(item: FoodItem, isChecked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+    Surface(
+        color = Color.White,
+        elevation = 8.dp,
+        modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        ) {
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = onCheckedChange,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp)
+            ) {
+                Text(text = item.name)
+                Text(text = "$${item.price}")
+            }
+        }
     }
 }
 
 @Composable
 fun FunScreen() {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Text(text = "Fun Tab Content")
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun AddItemScreen(navController: NavController) {
     var name by remember { mutableStateOf(TextFieldValue()) }
-    var canteen by remember { mutableStateOf(TextFieldValue()) }
     var price by remember { mutableStateOf(TextFieldValue()) }
-    var cuisine by remember { mutableStateOf(TextFieldValue()) }
-    var expanded by remember { mutableStateOf(false) }
-    var selectedCategory by remember { mutableStateOf("Main Course") }
-
-    val categories = listOf(
-        "Main Course",
-        "Beverage",
-        "Snacks"
-    )
 
     Scaffold(
         topBar = {
@@ -212,89 +271,23 @@ fun AddItemScreen(navController: NavController) {
             ) {
                 TextField(
                     value = name.text,
-                    onValueChange = { name = name.copy(text = it) },
+                    onValueChange = { name = it },
                     label = { Text("Name") },
-                    modifier = Modifier.fillMaxWidth().padding(8.dp)
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                TextField(
-                    value = canteen.text,
-                    onValueChange = { canteen = canteen.copy(text = it) },
-                    label = { Text("Canteen") },
                     modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.height(8.dp))
                 TextField(
                     value = price.text,
-                    onValueChange = { price = price.copy(text = it) },
+                    onValueChange = { price = it },
                     label = { Text("Price") },
                     modifier = Modifier.fillMaxWidth()
                 )
-                Spacer(modifier = Modifier.height(8.dp))
-                TextField(
-                    value = cuisine.text,
-                    onValueChange = { cuisine = cuisine.copy(text = it) },
-                    label = { Text("Cuisine") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                /*DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    categories.forEach { category ->
-                        DropdownMenuItem(
-                            onClick = {
-                                selectedCategory = category
-                                expanded = false
-                            }
-                        ) {
-                            Text(text = category)
-                        }
-                    }
-                }*/
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        text = "Category: $selectedCategory",
-                        modifier = Modifier.weight(1f).padding(end = 8.dp)
-                    )
-                    IconButton(
-                        onClick = { expanded = true },
-                        modifier = Modifier.align(Alignment.CenterVertically)
-                    ) {
-                        Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown")
-                    }
-                }
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    categories.forEach { category ->
-                        DropdownMenuItem(
-                            onClick = {
-                                selectedCategory = category
-                                expanded = false // Close the dropdown after selection
-                            }
-                        ) {
-                            Text(text = category)
-                        }
-                    }
-                }
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(
                     onClick = {
-                        saveDataToCsv(
-                            name.text,
-                            canteen.text,
-                            price.text,
-                            cuisine.text,
-                            selectedCategory
-                        )
+                        val newItem = FoodItem(name = name.text, price = price.text.toIntOrNull() ?: 0)
+                        writeFoodItemsToFile(LocalContext.current, listOf(newItem), "food_items.csv")
+                        navController.popBackStack()
                     },
                     modifier = Modifier.align(Alignment.CenterHorizontally)
                 ) {
@@ -305,17 +298,41 @@ fun AddItemScreen(navController: NavController) {
     )
 }
 
-fun saveDataToCsv(name: String, canteen: String, price: String, cuisine: String, category: String) {
-    val fileName = "yumpty_data.csv"
-    val csvHeader = "Name,Canteen,Price,Cuisine,Category\n"
-    val csvRow = "$name,$canteen,$price,$cuisine,$category\n"
-
-    val file = File(Environment.getExternalStorageDirectory(), fileName)
-    if (!file.exists()) {
-        file.createNewFile()
-        file.appendText(csvHeader)
+private fun writeFoodItemsToFile(
+    context: Context,
+    foodItems: List<FoodItem>,
+    fileName: String
+) {
+    val file = File(context.filesDir, fileName)
+    try {
+        FileOutputStream(file, true).bufferedWriter().use { writer ->
+            foodItems.forEach { item ->
+                writer.write("${item.name},${item.price}\n")
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
     }
-    file.appendText(csvRow)
+}
+
+private fun readFoodItemsFromFile(context: Context, fileName: String): List<FoodItem> {
+    val file = File(context.filesDir, fileName)
+    val foodItems = mutableListOf<FoodItem>()
+    try {
+        file.bufferedReader().useLines { lines ->
+            lines.forEach { line ->
+                val parts = line.split(",")
+                if (parts.size == 2) {
+                    val name = parts[0]
+                    val price = parts[1].toIntOrNull() ?: 0
+                    foodItems.add(FoodItem(name, price))
+                }
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return foodItems
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -325,5 +342,36 @@ fun DefaultPreview() {
     YumptyTheme {
         val navController = rememberNavController()
         Yumpty(navController)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FoodScreenPreview() {
+    val foodItems = listOf(
+        FoodItem("Pizza", 12),
+        FoodItem("Burger", 8),
+        FoodItem("Salad", 6)
+    )
+    YumptyTheme {
+        FoodScreen(foodItems = foodItems)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FunScreenPreview() {
+    YumptyTheme {
+        FunScreen()
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview(showBackground = true)
+@Composable
+fun AddItemScreenPreview() {
+    YumptyTheme {
+        val navController = rememberNavController()
+        AddItemScreen(navController = navController)
     }
 }
